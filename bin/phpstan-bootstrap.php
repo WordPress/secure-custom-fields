4d0e4eb2abc07653e6cd3a59413ae521fa36c7f0
Brandon Kraft <public@brandonkraft.com>
2025-03-04 16:01:46 -0600

    Update baseline after adding entrypoint

---
+++ bin/phpstan-bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Bootstrap the plugin for PHPStan.
+ *
+ * Loads the plugin file as the entry point for PHPStan.
+ *
+ * @package WordPress/Secure-Custom-Fields
+ */
+
+/**
+ * Main plugin file. Will always be loaded.
+ */
+require_once __DIR__ . '/../secure-custom-fields.php';
