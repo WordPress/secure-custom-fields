6a804861fcb33e807efbc95d81e28f883546349a
Yan Metelitsa <yan.metelitsa@gmail.com>
2025-03-28 19:02:41 +0300

    Fix empty shortcode output

--- includes/api/api-template.php
+++ includes/api/api-template.php
@@ -1084,7 +1084,8 @@ function acf_shortcode( $atts ) {
 		if ( is_preview() ) {
 			return apply_filters( 'acf/shortcode/field_not_allowed_message', '[' . esc_html__( 'The requested ACF field is not allowed to be output in bindings or the ACF Shortcode.', 'secure-custom-fields' ) . ']' );
 		} else {
-			return apply_filters( 'acf/shortcode/field_not_allowed_message', '[' . sprintf( esc_html__( 'You need to enable "Allow Access to Value in Editor UI" in the "%s" field parameters to display it in the ACF Shortcode.', 'secure-custom-fields' ), $field[ 'label' ] ) . ']' );
+			/* translators: %s Field name */
+			return apply_filters( 'acf/shortcode/field_not_allowed_message', '[' . sprintf( esc_html__( 'You need to enable "Allow Access to Value in Editor UI" in the "%s" field parameters to display it in the ACF Shortcode.', 'secure-custom-fields' ), $field[ 'name' ] ) . ']' );
 		}
 	}
 
