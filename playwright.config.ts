4c20a04fe5b23de1424cdb65e8d4563f9bc07906
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2025-04-01 15:23:25 +0200

    Full test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,8 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 const baseConfig = require( '@wordpress/scripts/config/playwright.config.js' );
 const config = defineConfig( {
   ...baseConfig,
   testDir: './tests/e2e',
-  projects: [
-		{
-			name: 'chromium',
-			use: { ...devices[ 'Desktop Chrome' ] },
-			grepInvert: /-chromium/,
-		},
-	],
 } );
 export default config;
