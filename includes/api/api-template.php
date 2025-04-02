7a8df68a3e001ad654199951c646bdf6693e3f44
Brandon Kraft <public@brandonkraft.com>
2025-04-02 08:41:37 -0500

    phpcs

--- includes/api/api-template.php
+++ includes/api/api-template.php
@@ -1085,7 +1085,7 @@ function acf_shortcode( $atts ) {
 			return apply_filters( 'acf/shortcode/field_not_allowed_message', '[' . esc_html__( 'The requested ACF field is not allowed to be output in bindings or the ACF Shortcode.', 'secure-custom-fields' ) . ']' );
 		} else {
 			/* translators: %s Field name */
-			return apply_filters( 'acf/shortcode/field_not_allowed_message', '[' . sprintf( esc_html__( 'You need to enable "Allow Access to Value in Editor UI" in the "%s" field parameters to display it in the ACF Shortcode.', 'secure-custom-fields' ), $field[ 'name' ] ) . ']' );
+			return apply_filters( 'acf/shortcode/field_not_allowed_message', '[' . sprintf( esc_html__( 'You need to enable "Allow Access to Value in Editor UI" in the "%s" field parameters to display it in the ACF Shortcode.', 'secure-custom-fields' ), $field['name'] ) . ']' );
 		}
 	}
 
