8bb09f78b0f7fc13f7f967127cf89657a05c6600
Paul Kevan <2290623+pkevan@users.noreply.github.com>
2025-04-17 11:26:28 +0100

    Return to correct directory
    When running `update_translations` we end up in a different directory (https://github.com/WordPress/secure-custom-fields/blob/c037ce16b9a110115af2289053d87430d78eec97/bin/update-translations.php#L16), this resets that to where it is expected.

--- bin/prepare-release.php
+++ bin/prepare-release.php
@@ -50,6 +50,8 @@ class Release_Preparation {
 		$this->run_tests();
 		$this->generate_docs();
 		$this->update_translations();
+		// need to return to correct directory after running `update_translations`.
+		exec( chdir( dirname( __DIR__ ) ) );
 		$this->commit_changes();
 
 		$current_version = $this->get_current_version();
