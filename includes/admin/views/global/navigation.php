1050e0c7b131568af82e3a7224e6b6464e2d58d4
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2025-04-21 17:58:46 +0200

    Fix header

--- includes/admin/views/global/navigation.php
+++ includes/admin/views/global/navigation.php
@@ -173,8 +173,10 @@ global $plugin_page;
 $screen = get_current_screen();
 
 if ( ! in_array( $screen->id, acf_get_internal_post_types(), true ) ) {
-	if ( $plugin_page == 'acf-tools' ) {
+	if ( 'acf-tools' === $plugin_page ) {
 		$acf_page_title = __( 'Tools', 'secure-custom-fields' );
+	} elseif ( 'scf-experiments' === $plugin_page ) {
+		$acf_page_title = __( 'Experiments', 'secure-custom-fields' );
 	}
 	acf_get_view( 'global/header' );
 }
