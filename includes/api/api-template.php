e03be1f50e975b925524b879e5601b41f1595141
Paul Kevan <2290623+pkevan@users.noreply.github.com>
2025-04-23 10:36:54 +0100

    phpcs

--- includes/api/api-template.php
+++ includes/api/api-template.php
@@ -1084,7 +1084,7 @@ function acf_shortcode( $atts ) {
 	}
 
 	if ( isset( $field['allow_in_bindings'] ) && ! $field['allow_in_bindings'] ) {
-		if ( is_preview() || current_user_can( $display_errors_to_user )) {
+		if ( is_preview() || current_user_can( $display_errors_to_user ) ) {
 			return apply_filters( 'acf/shortcode/field_not_allowed_message', '[' . esc_html__( 'The requested ACF field is not allowed to be output in bindings or the ACF Shortcode.', 'secure-custom-fields' ) . ']' );
 		} else {
 			return;
