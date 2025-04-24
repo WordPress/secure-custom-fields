4c6f5da4b548c16dd0ac9dcce16a35345ba30c07
priethor <27339341+priethor@users.noreply.github.com>
2025-04-24 14:50:02 +0200

    Apply linting

--- assets/src/js/acf-command-palette-core.js
+++ assets/src/js/acf-command-palette-core.js
@@ -2,19 +2,23 @@
  * SCF Core Command Palette integration
  *
  * Core WordPress admin commands for Secure Custom Fields.
- * 
+ *
  * @since 6.5.0
  */
 
 // Register commands when WordPress is ready
-wp.domReady(() => {
+wp.domReady( () => {
 	// Make sure required WordPress dependencies are available
-	if (!wp.data || !wp.data.dispatch || !wp.data.dispatch('core/commands')) {
+	if (
+		! wp.data ||
+		! wp.data.dispatch ||
+		! wp.data.dispatch( 'core/commands' )
+	) {
 		return;
 	}
 
 	// Wait for ACF to be ready
-	if (typeof acf === 'undefined') {
+	if ( typeof acf === 'undefined' ) {
 		return;
 	}
 
@@ -22,116 +26,167 @@ wp.domReady(() => {
 	const { __ } = wp.i18n;
 	const { createElement } = wp.element;
 	const { Icon } = wp.components;
-	const commandStore = wp.data.dispatch('core/commands');
-	
+	const commandStore = wp.data.dispatch( 'core/commands' );
+
 	// Get data from ACF object
 	const adminUrl = acf?.data?.admin_url || '';
 
 	// Core command definitions for SCF admin pages
 	const commands = [
-		{ 
-			name: 'field-groups', 
-			label: __('Field Groups', 'secure-custom-fields'), 
-			url: `${adminUrl}edit.php?post_type=acf-field-group`,
+		{
+			name: 'field-groups',
+			label: __( 'Field Groups', 'secure-custom-fields' ),
+			url: `${ adminUrl }edit.php?post_type=acf-field-group`,
 			icon: 'layout',
-			description: __('SCF: View and manage custom field groups', 'secure-custom-fields'),
-			keywords: ['acf', 'custom fields', 'field editor', 'manage fields']
+			description: __(
+				'SCF: View and manage custom field groups',
+				'secure-custom-fields'
+			),
+			keywords: [
+				'acf',
+				'custom fields',
+				'field editor',
+				'manage fields',
+			],
 		},
-		{ 
-			name: 'new-field-group', 
-			label: __('Create New Field Group', 'secure-custom-fields'), 
-			url: `${adminUrl}post-new.php?post_type=acf-field-group`,
+		{
+			name: 'new-field-group',
+			label: __( 'Create New Field Group', 'secure-custom-fields' ),
+			url: `${ adminUrl }post-new.php?post_type=acf-field-group`,
 			icon: 'plus',
-			description: __('SCF: Create a new field group to organize custom fields', 'secure-custom-fields'),
-			keywords: ['add', 'new', 'create', 'field group', 'custom fields']
+			description: __(
+				'SCF: Create a new field group to organize custom fields',
+				'secure-custom-fields'
+			),
+			keywords: [
+				'add',
+				'new',
+				'create',
+				'field group',
+				'custom fields',
+			],
 		},
-		{ 
-			name: 'post-types', 
-			label: __('Post Types', 'secure-custom-fields'), 
-			url: `${adminUrl}edit.php?post_type=acf-post-type`,
+		{
+			name: 'post-types',
+			label: __( 'Post Types', 'secure-custom-fields' ),
+			url: `${ adminUrl }edit.php?post_type=acf-post-type`,
 			icon: 'admin-post',
-			description: __('SCF: Manage custom post types', 'secure-custom-fields'),
-			keywords: ['cpt', 'content types', 'manage post types']
+			description: __(
+				'SCF: Manage custom post types',
+				'secure-custom-fields'
+			),
+			keywords: [ 'cpt', 'content types', 'manage post types' ],
 		},
-		{ 
-			name: 'new-post-type', 
-			label: __('Create New Post Type', 'secure-custom-fields'), 
-			url: `${adminUrl}post-new.php?post_type=acf-post-type`,
+		{
+			name: 'new-post-type',
+			label: __( 'Create New Post Type', 'secure-custom-fields' ),
+			url: `${ adminUrl }post-new.php?post_type=acf-post-type`,
 			icon: 'plus',
-			description: __('SCF: Create a new custom post type', 'secure-custom-fields'),
-			keywords: ['add', 'new', 'create', 'cpt', 'content type']
+			description: __(
+				'SCF: Create a new custom post type',
+				'secure-custom-fields'
+			),
+			keywords: [ 'add', 'new', 'create', 'cpt', 'content type' ],
 		},
-		{ 
-			name: 'taxonomies', 
-			label: __('Taxonomies', 'secure-custom-fields'), 
-			url: `${adminUrl}edit.php?post_type=acf-taxonomy`,
+		{
+			name: 'taxonomies',
+			label: __( 'Taxonomies', 'secure-custom-fields' ),
+			url: `${ adminUrl }edit.php?post_type=acf-taxonomy`,
 			icon: 'category',
-			description: __('SCF: Manage custom taxonomies for organizing content', 'secure-custom-fields'),
-			keywords: ['categories', 'tags', 'terms', 'custom taxonomies']
+			description: __(
+				'SCF: Manage custom taxonomies for organizing content',
+				'secure-custom-fields'
+			),
+			keywords: [ 'categories', 'tags', 'terms', 'custom taxonomies' ],
 		},
-		{ 
-			name: 'new-taxonomy', 
-			label: __('Create New Taxonomy', 'secure-custom-fields'), 
-			url: `${adminUrl}post-new.php?post_type=acf-taxonomy`,
+		{
+			name: 'new-taxonomy',
+			label: __( 'Create New Taxonomy', 'secure-custom-fields' ),
+			url: `${ adminUrl }post-new.php?post_type=acf-taxonomy`,
 			icon: 'plus',
-			description: __('SCF: Create a new custom taxonomy', 'secure-custom-fields'),
-			keywords: ['add', 'new', 'create', 'taxonomy', 'categories', 'tags']
+			description: __(
+				'SCF: Create a new custom taxonomy',
+				'secure-custom-fields'
+			),
+			keywords: [
+				'add',
+				'new',
+				'create',
+				'taxonomy',
+				'categories',
+				'tags',
+			],
 		},
-		{ 
-			name: 'options-pages', 
-			label: __('Options Pages', 'secure-custom-fields'), 
-			url: `${adminUrl}edit.php?post_type=acf-ui-options-page`,
+		{
+			name: 'options-pages',
+			label: __( 'Options Pages', 'secure-custom-fields' ),
+			url: `${ adminUrl }edit.php?post_type=acf-ui-options-page`,
 			icon: 'admin-settings',
-			description: __('SCF: Manage custom options pages for global settings', 'secure-custom-fields'),
-			keywords: ['settings', 'global options', 'site options']
+			description: __(
+				'SCF: Manage custom options pages for global settings',
+				'secure-custom-fields'
+			),
+			keywords: [ 'settings', 'global options', 'site options' ],
 		},
-		{ 
-			name: 'new-options-page', 
-			label: __('Create New Options Page', 'secure-custom-fields'), 
-			url: `${adminUrl}post-new.php?post_type=acf-ui-options-page`,
+		{
+			name: 'new-options-page',
+			label: __( 'Create New Options Page', 'secure-custom-fields' ),
+			url: `${ adminUrl }post-new.php?post_type=acf-ui-options-page`,
 			icon: 'plus',
-			description: __('SCF: Create a new custom options page', 'secure-custom-fields'),
-			keywords: ['add', 'new', 'create', 'options', 'settings page']
+			description: __(
+				'SCF: Create a new custom options page',
+				'secure-custom-fields'
+			),
+			keywords: [ 'add', 'new', 'create', 'options', 'settings page' ],
 		},
-		{ 
-			name: 'tools', 
-			label: __('SCF Tools', 'secure-custom-fields'), 
-			url: `${adminUrl}admin.php?page=acf-tools`,
+		{
+			name: 'tools',
+			label: __( 'SCF Tools', 'secure-custom-fields' ),
+			url: `${ adminUrl }admin.php?page=acf-tools`,
 			icon: 'admin-tools',
-			description: __('SCF: Access SCF utility tools', 'secure-custom-fields'),
-			keywords: ['utilities', 'import export', 'json']
+			description: __(
+				'SCF: Access SCF utility tools',
+				'secure-custom-fields'
+			),
+			keywords: [ 'utilities', 'import export', 'json' ],
 		},
-		{ 
-			name: 'import', 
-			label: __('Import SCF Data', 'secure-custom-fields'), 
-			url: `${adminUrl}admin.php?page=acf-tools&tool=import`,
+		{
+			name: 'import',
+			label: __( 'Import SCF Data', 'secure-custom-fields' ),
+			url: `${ adminUrl }admin.php?page=acf-tools&tool=import`,
 			icon: 'upload',
-			description: __('SCF: Import field groups, post types, taxonomies, and options pages', 'secure-custom-fields'),
-			keywords: ['upload', 'json', 'migration', 'transfer']
+			description: __(
+				'SCF: Import field groups, post types, taxonomies, and options pages',
+				'secure-custom-fields'
+			),
+			keywords: [ 'upload', 'json', 'migration', 'transfer' ],
 		},
-		{ 
-			name: 'export', 
-			label: __('Export SCF Data', 'secure-custom-fields'), 
-			url: `${adminUrl}admin.php?page=acf-tools&tool=export`,
+		{
+			name: 'export',
+			label: __( 'Export SCF Data', 'secure-custom-fields' ),
+			url: `${ adminUrl }admin.php?page=acf-tools&tool=export`,
 			icon: 'download',
-			description: __('SCF: Export field groups, post types, taxonomies, and options pages', 'secure-custom-fields'),
-			keywords: ['download', 'json', 'backup', 'migration']
-		}
+			description: __(
+				'SCF: Export field groups, post types, taxonomies, and options pages',
+				'secure-custom-fields'
+			),
+			keywords: [ 'download', 'json', 'backup', 'migration' ],
+		},
 	];
-	
+
 	// Register each command
-	commands.forEach(command => {
-		commandStore.registerCommand({
+	commands.forEach( ( command ) => {
+		commandStore.registerCommand( {
 			name: 'scf/' + command.name,
 			label: command.label,
-			icon: createElement(Icon, { icon: command.icon }),
+			icon: createElement( Icon, { icon: command.icon } ),
 			context: 'admin',
 			description: command.description,
 			keywords: command.keywords,
-			callback: ({ close }) => {
+			callback: ( { close } ) => {
 				document.location = command.url;
 				close();
-			}
-		});
-	});
-}); 
+			},
+		} );
+	} );
+} );
--- assets/src/js/acf-command-palette-post-types.js
+++ assets/src/js/acf-command-palette-post-types.js
@@ -2,19 +2,23 @@
  * SCF Dynamic Post Type Command Palette integration
  *
  * Dynamic commands for user-created custom post types in Secure Custom Fields.
- * 
+ *
  * @since 6.5.0
  */
 
 // Register commands when WordPress is ready
-wp.domReady(() => {
+wp.domReady( () => {
 	// Make sure required WordPress dependencies are available
-	if (!wp.data || !wp.data.dispatch || !wp.data.dispatch('core/commands')) {
+	if (
+		! wp.data ||
+		! wp.data.dispatch ||
+		! wp.data.dispatch( 'core/commands' )
+	) {
 		return;
 	}
 
 	// Wait for ACF to be ready
-	if (typeof acf === 'undefined') {
+	if ( typeof acf === 'undefined' ) {
 		return;
 	}
 
@@ -22,52 +26,70 @@ wp.domReady(() => {
 	const { __ } = wp.i18n;
 	const { createElement } = wp.element;
 	const { Icon } = wp.components;
-	const commandStore = wp.data.dispatch('core/commands');
-	
+	const commandStore = wp.data.dispatch( 'core/commands' );
+
 	// Get data from ACF object
 	const adminUrl = acf?.data?.admin_url || '';
 	const postTypes = acf?.data?.customPostTypes || [];
 
 	// Skip if no custom post types
-	if (!postTypes || postTypes.length === 0) {
+	if ( ! postTypes || postTypes.length === 0 ) {
 		return;
 	}
 
 	// Add commands for each custom post type
-	postTypes.forEach(postType => {
+	postTypes.forEach( ( postType ) => {
 		// Skip invalid post types
-		if (!postType?.name) return;
-		
+		if ( ! postType?.name ) return;
+
 		// Get labels
 		const pluralLabel = postType.label || postType.name;
 		const singularLabel = postType.singular_label || pluralLabel;
-		
+
 		// Add command to view all posts of this type
-		commandStore.registerCommand({
-			name: `scf/cpt-${postType.name}`,
+		commandStore.registerCommand( {
+			name: `scf/cpt-${ postType.name }`,
 			label: pluralLabel,
-			icon: createElement(Icon, { icon: 'admin-page' }),
+			icon: createElement( Icon, { icon: 'admin-page' } ),
 			context: 'admin',
-			description: __('SCF: View all', 'secure-custom-fields') + ` ${pluralLabel}`,
-			keywords: ['post type', 'content', 'cpt', postType.name, postType.label || ''],
-			callback: ({ close }) => {
-				document.location = `${adminUrl}edit.php?post_type=${postType.name}`;
+			description:
+				__( 'SCF: View all', 'secure-custom-fields' ) +
+				` ${ pluralLabel }`,
+			keywords: [
+				'post type',
+				'content',
+				'cpt',
+				postType.name,
+				postType.label || '',
+			],
+			callback: ( { close } ) => {
+				document.location = `${ adminUrl }edit.php?post_type=${ postType.name }`;
 				close();
-			}
-		});
-		
+			},
+		} );
+
 		// Add "new post" command (all included post types are editable by current user)
-		commandStore.registerCommand({
-			name: `scf/new-${postType.name}`,
-			label: __('Add New', 'secure-custom-fields') + ` ${singularLabel}`,
-			icon: createElement(Icon, { icon: 'plus' }),
+		commandStore.registerCommand( {
+			name: `scf/new-${ postType.name }`,
+			label:
+				__( 'Add New', 'secure-custom-fields' ) + ` ${ singularLabel }`,
+			icon: createElement( Icon, { icon: 'plus' } ),
 			context: 'admin',
-			description: __('SCF: Create a new', 'secure-custom-fields') + ` ${singularLabel}`,
-			keywords: ['add', 'new', 'create', 'content', postType.name, postType.label || ''],
-			callback: ({ close }) => {
-				document.location = `${adminUrl}post-new.php?post_type=${postType.name}`;
+			description:
+				__( 'SCF: Create a new', 'secure-custom-fields' ) +
+				` ${ singularLabel }`,
+			keywords: [
+				'add',
+				'new',
+				'create',
+				'content',
+				postType.name,
+				postType.label || '',
+			],
+			callback: ( { close } ) => {
+				document.location = `${ adminUrl }post-new.php?post_type=${ postType.name }`;
 				close();
-			}
-		});
-	});
-}); 
+			},
+		} );
+	} );
+} );
