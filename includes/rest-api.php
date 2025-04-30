4938c717cc1164b432df8f1e653746a3c3b4cb85
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2025-04-30 10:19:43 +0200

    Default should be  false

--- includes/rest-api.php
+++ includes/rest-api.php
@@ -20,6 +20,6 @@ acf_include( 'includes/rest-api/class-acf-rest-types-endpoint.php' );
 // Initialize.
 acf_new_instance( 'ACF_Rest_Api' );
 
-// Always initialize SCF_Rest_Types_Endpoint
-// The class will internally check for the beta feature status for field groups
+// Always initialize SCF_Rest_Types_Endpoint for the origin parameter functionality
+// The class will internally check for the beta feature status for field groups registration
 acf_new_instance( 'SCF_Rest_Types_Endpoint' );
