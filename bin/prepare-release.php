33bf4f5a789ad49c95ec3559c259c33192b6827b
Brandon Kraft <public@brandonkraft.com>
2025-05-07 04:41:51 +0100

    Correct stable release generation (#108)

--- bin/prepare-release.php
+++ bin/prepare-release.php
@@ -304,7 +304,7 @@ class Release_Preparation {
 	private function update_stable_tag( $version ) {
 		$readme = file_get_contents( 'readme.txt' );
 		$readme = preg_replace(
-			'/^Stable tag:.*$/m',
+			'/^Stable tag:\s*.*$/m',
 			'Stable tag: ' . $version,
 			$readme
 		);
