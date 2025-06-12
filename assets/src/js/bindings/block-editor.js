464fa1f8cf5b86ea6dc3ceda2641df1412a32780
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2025-06-12 12:26:01 +0200

    First commit

---
+++ assets/src/js/bindings/block-editor.js
@@ -0,0 +1,129 @@
+/**
+ * WordPress dependencies
+ */
+import { addFilter } from '@wordpress/hooks';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, MenuGroup, FormTokenField } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
+import { store as coreDataStore } from '@wordpress/core-data';
+import { store as editorStore } from '@wordpress/editor';
+import { store as blockEditorStore } from '@wordpress/block-editor';
+
+const BLOCK_BINDINGS_ALLOWED_BLOCKS = {
+	'core/paragraph': [ 'content' ],
+	'core/heading': [ 'content' ],
+	'core/image': [ 'id', 'url', 'title', 'alt' ],
+	'core/button': [ 'url', 'text', 'linkTarget', 'rel' ],
+};
+
+/**
+ * Gets the bindable attributes for a given block.
+ *
+ * @param {string} blockName The name of the block.
+ *
+ * @return {string[]} The bindable attributes for the block.
+ */
+function getBindableAttributes( blockName ) {
+	return BLOCK_BINDINGS_ALLOWED_BLOCKS[ blockName ];
+}
+
+/**
+ * Add custom attributes to all blocks
+ */
+const addCustomAttributes = ( settings ) => {
+	// Add custom attribute to all blocks
+	if ( settings.attributes ) {
+		settings.attributes.customAttribute = {
+			type: 'string',
+			default: '',
+		};
+	}
+	return settings;
+};
+
+addFilter(
+	'blocks.registerBlockType',
+	'secure-custom-fields/add-custom-attributes',
+	addCustomAttributes
+);
+
+/**
+ * Add custom controls to all blocks
+ */
+const withCustomControls = createHigherOrderComponent( ( BlockEdit ) => {
+	return ( props ) => {
+		const bindableAttributes = getBindableAttributes( props.name );
+
+		const { postType, postId } = useSelect( ( select ) => {
+			const { getCurrentPostType, getCurrentPostId } =
+				select( editorStore );
+			return {
+				postType: getCurrentPostType(),
+				postId: getCurrentPostId(),
+			};
+		}, [] );
+
+		const fields = useSelect(
+			( select ) => {
+				const { getEditedEntityRecord } = select( coreDataStore );
+
+				if ( ! postType || ! postId ) {
+					return undefined;
+				}
+
+				const record = getEditedEntityRecord(
+					'postType',
+					postType,
+					postId
+				);
+				return record?.acf;
+			},
+			[ postType, postId ]
+		);
+
+		const fieldsSuggestions = Object.keys( fields );
+
+		return (
+			<>
+				<BlockEdit { ...props } />
+				<InspectorControls>
+					<PanelBody
+						title={ __(
+							'Connect to a field',
+							'secure-custom-fields'
+						) }
+						initialOpen={ true }
+					>
+						<MenuGroup>
+							{ bindableAttributes.map( ( attribute ) => (
+								<FormTokenField
+									__next40pxDefaultSize
+									__nextHasNoMarginBottom
+									__experimentalShowHowTo={ false }
+									__experimentalExpandOnFocus={ true }
+									__experimentalAutoSelectFirstMatch={ true }
+									label={ attribute }
+									maxLength={ 1 }
+									onChange={ ( value ) => {
+										console.log( 'change' );
+										console.log( value );
+									} }
+									suggestions={ fieldsSuggestions }
+									value={ [] }
+								/>
+							) ) }
+						</MenuGroup>
+					</PanelBody>
+				</InspectorControls>
+			</>
+		);
+	};
+}, 'withCustomControls' );
+
+addFilter(
+	'editor.BlockEdit',
+	'secure-custom-fields/with-custom-controls',
+	withCustomControls
+);
