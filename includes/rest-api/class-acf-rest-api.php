b2d71af4ebe2ffe9443e02847f01180b79e94381
Phi Phan <mrphipv@gmail.com>
2025-08-30 14:44:21 +0700

    Get the formatted_value from the original field value
    The formatted_value property of the $field['name'] . '_source' should come from the original value, not the REST value. Other plugins might change the REST value using the filter acf/rest/format_value_for_rest, which can cause the formatted_value to be incorrect.

--- includes/rest-api/class-acf-rest-api.php
+++ includes/rest-api/class-acf-rest-api.php
@@ -231,8 +231,8 @@ class ACF_Rest_Api {
 				}
 
 				// Format the field value according to the request params.
-				$format = $request->get_param( 'acf_format' ) ?: acf_get_setting( 'rest_api_format' );
-				$rest_value  = acf_format_value_for_rest( $value, $post_id, $field, $format );
+				$format     = $request->get_param( 'acf_format' ) ?: acf_get_setting( 'rest_api_format' );
+				$rest_value = acf_format_value_for_rest( $value, $post_id, $field, $format );
 
 				// We keep this one for backward compatibility with existing code that expects the field value to be.
 				$fields[ $field['name'] ]             = $rest_value;
