ed9dc84db5ed8c9007c6d88cba40f66c775b9159
priethor <27339341+priethor@users.noreply.github.com>
2025-12-22 15:22:56 +0100

    Add tests

---
+++ tests/php/post-types/SCFFieldManagerTest.php
@@ -0,0 +1,287 @@
+<?php
+/**
+ * Tests for SCF_Field_Manager class.
+ *
+ * @package wordpress/secure-custom-fields
+ */
+
+use WorDBless\BaseTestCase;
+
+require_once dirname( __DIR__, 3 ) . '/includes/post-types/class-scf-field-manager.php';
+
+/**
+ * SCF_Field_Manager test case.
+ */
+class SCFFieldManagerTest extends BaseTestCase {
+
+	/**
+	 * Manager instance.
+	 *
+	 * @var SCF_Field_Manager
+	 */
+	private $manager;
+
+	/**
+	 * Sample fields.
+	 *
+	 * @var array
+	 */
+	private $sample_fields;
+
+	/**
+	 * Set up.
+	 */
+	public function setUp(): void {
+		parent::setUp();
+		$this->manager       = new SCF_Field_Manager();
+		$this->sample_fields = array(
+			array(
+				'key'    => 'field_1',
+				'name'   => 'text_field',
+				'type'   => 'text',
+				'parent' => 'group_123',
+			),
+			array(
+				'key'    => 'field_2',
+				'name'   => 'image_field',
+				'type'   => 'image',
+				'parent' => 'group_123',
+			),
+			array(
+				'key'    => 'field_3',
+				'name'   => 'another_text',
+				'type'   => 'text',
+				'parent' => 'group_456',
+			),
+			array(
+				'key'    => 'field_4',
+				'name'   => 'email_field',
+				'type'   => 'email',
+				'parent' => 'group_456',
+			),
+		);
+	}
+
+	/**
+	 * Create a test field.
+	 *
+	 * @param string $key Field key suffix.
+	 * @return array
+	 */
+	private function create_test_field( $key ) {
+		return acf_update_field(
+			array(
+				'key'   => 'field_' . $key,
+				'name'  => $key,
+				'type'  => 'text',
+				'label' => ucfirst( $key ),
+			)
+		);
+	}
+
+	/**
+	 * Test filter_posts with no filters.
+	 */
+	public function test_filter_posts_no_filters() {
+		$result = $this->manager->filter_posts( $this->sample_fields );
+		$this->assertCount( 4, $result );
+	}
+
+	/**
+	 * Test filter_posts by parent.
+	 */
+	public function test_filter_posts_by_parent() {
+		$result = $this->manager->filter_posts( $this->sample_fields, array( 'parent' => 'group_123' ) );
+
+		$this->assertCount( 2, $result );
+		foreach ( $result as $field ) {
+			$this->assertEquals( 'group_123', $field['parent'] );
+		}
+	}
+
+	/**
+	 * Test filter_posts by type.
+	 */
+	public function test_filter_posts_by_type() {
+		$result = $this->manager->filter_posts( $this->sample_fields, array( 'type' => 'text' ) );
+
+		$this->assertCount( 2, $result );
+		foreach ( $result as $field ) {
+			$this->assertEquals( 'text', $field['type'] );
+		}
+	}
+
+	/**
+	 * Test filter_posts by name.
+	 */
+	public function test_filter_posts_by_name() {
+		$result = $this->manager->filter_posts( $this->sample_fields, array( 'name' => 'email_field' ) );
+
+		$this->assertCount( 1, $result );
+		$this->assertEquals( 'email_field', $result[0]['name'] );
+	}
+
+	/**
+	 * Test filter_posts with multiple filters.
+	 */
+	public function test_filter_posts_multiple_filters() {
+		$result = $this->manager->filter_posts(
+			$this->sample_fields,
+			array(
+				'parent' => 'group_123',
+				'type'   => 'text',
+			)
+		);
+
+		$this->assertCount( 1, $result );
+		$this->assertEquals( 'field_1', $result[0]['key'] );
+	}
+
+	/**
+	 * Test filter_posts with no matches.
+	 */
+	public function test_filter_posts_no_matches() {
+		$result = $this->manager->filter_posts( $this->sample_fields, array( 'type' => 'nonexistent' ) );
+		$this->assertCount( 0, $result );
+	}
+
+	/**
+	 * Test filter_posts reindexes keys.
+	 */
+	public function test_filter_posts_reindexes_keys() {
+		$result = $this->manager->filter_posts( $this->sample_fields, array( 'parent' => 'group_456' ) );
+
+		$this->assertArrayHasKey( 0, $result );
+		$this->assertArrayHasKey( 1, $result );
+		$this->assertArrayNotHasKey( 2, $result );
+	}
+
+	/**
+	 * Test filter_posts with empty input.
+	 */
+	public function test_filter_posts_empty_input() {
+		$result = $this->manager->filter_posts( array(), array( 'type' => 'text' ) );
+		$this->assertCount( 0, $result );
+	}
+
+	/**
+	 * Test post_type property.
+	 */
+	public function test_post_type_property() {
+		$this->assertEquals( 'acf-field', $this->manager->post_type );
+	}
+
+	/**
+	 * Test post_key_prefix property.
+	 */
+	public function test_post_key_prefix_property() {
+		$this->assertEquals( 'field_', $this->manager->post_key_prefix );
+	}
+
+	/**
+	 * Test duplicate_post returns false when field not found.
+	 */
+	public function test_duplicate_post_returns_false_when_field_not_found() {
+		$this->assertFalse( $this->manager->duplicate_post( 'nonexistent_field' ) );
+	}
+
+	/**
+	 * Test get_post calls acf_get_field.
+	 */
+	public function test_get_post_calls_acf_get_field() {
+		$field       = $this->create_test_field( 'get_test' );
+		$called_with = null;
+
+		add_filter(
+			'acf/load_field',
+			function ( $f ) use ( &$called_with ) {
+				$called_with = $f;
+				return $f;
+			}
+		);
+
+		$this->manager->get_post( $field['ID'] );
+		$this->assertEquals( 'field_get_test', $called_with['key'] );
+	}
+
+	/**
+	 * Test update_post calls acf_update_field.
+	 */
+	public function test_update_post_calls_acf_update_field() {
+		$called_with = null;
+
+		add_filter(
+			'acf/update_field',
+			function ( $field ) use ( &$called_with ) {
+				$called_with = $field;
+				return $field;
+			}
+		);
+
+		$this->manager->update_post(
+			array(
+				'key'   => 'field_update_test',
+				'name'  => 'update_test',
+				'type'  => 'text',
+				'label' => 'Update Test',
+			)
+		);
+
+		$this->assertEquals( 'field_update_test', $called_with['key'] );
+	}
+
+	/**
+	 * Test delete_post calls acf_delete_field.
+	 */
+	public function test_delete_post_calls_acf_delete_field() {
+		$field  = $this->create_test_field( 'delete_test' );
+		$called = false;
+
+		add_action(
+			'acf/delete_field',
+			function () use ( &$called ) {
+				$called = true;
+			}
+		);
+
+		$this->manager->delete_post( $field['ID'] );
+		$this->assertTrue( $called );
+	}
+
+	/**
+	 * Test trash_post calls acf_trash_field.
+	 */
+	public function test_trash_post_calls_acf_trash_field() {
+		$field  = $this->create_test_field( 'trash_test' );
+		$called = false;
+
+		add_action(
+			'acf/trash_field',
+			function () use ( &$called ) {
+				$called = true;
+			}
+		);
+
+		$this->manager->trash_post( $field['ID'] );
+		$this->assertTrue( $called );
+	}
+
+	/**
+	 * Test untrash_post calls acf_untrash_field.
+	 */
+	public function test_untrash_post_calls_acf_untrash_field() {
+		$field = $this->create_test_field( 'untrash_test' );
+		acf_trash_field( $field['ID'] );
+		$called = false;
+
+		add_action(
+			'acf/untrash_field',
+			function () use ( &$called ) {
+				$called = true;
+			}
+		);
+
+		$this->manager->untrash_post( $field['ID'] );
+		$this->assertTrue( $called );
+	}
+}
